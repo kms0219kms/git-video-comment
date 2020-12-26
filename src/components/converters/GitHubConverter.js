9c267eb89df6317f8c4fc7355f857ed5cba2ce40
All PGS 모든 프로그래머들 <kms0219kms@hosue.goe.go.kr>
2020-12-26 19:44:33 +0900

    Update GitHubConverter.js

--- src/components/converters/GitHubConverter.js
+++ src/components/converters/GitHubConverter.js
@@ -4,7 +4,7 @@ import Converter from '../converter/Converter'
 const GitHubConverter = (props) => {
     return (
         <Converter header="convert Youtube url to GitHub comment">
-            <p>Hi, my name is <a href="https://sviridovserg.github.io">Sergei</a> I love delivering great product to the customer and help teams to grow! I'm using GitHub a lot. It is hard to embed Youtube video to GitHub markdown when you adding comment to pull request on GitHub or changing markdown files. So, I created this simple tool that converts Youtube video URI to the embeddable markup that can be simply copy-pasted to GitHub.</p>
+         <p>출처: https://github.com/sviridovserg/git-video-comment</
         </Converter>
     )
 }
