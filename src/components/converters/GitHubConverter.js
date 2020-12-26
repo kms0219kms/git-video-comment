30719cbc91d1404be7166c501dd9219d63bc6e0a
All PGS 모든 프로그래머들 <kms0219kms@hosue.goe.go.kr>
2020-12-26 19:50:10 +0900

    Update GitHubConverter.js

--- src/components/converters/GitHubConverter.js
+++ src/components/converters/GitHubConverter.js
@@ -3,7 +3,7 @@ import Converter from '../converter/Converter'
 
 const GitHubConverter = (props) => {
     return (
-        <Converter header="convert Youtube url to GitHub comment">
+        <Converter header="Youtube 링크를 Github Readme에 삽입하기">
          <p>출처: <a href="https://github.com/sviridovserg/git-video-comment">sviridovserg/git-video-comment</a></p>
         </Converter>
     )
