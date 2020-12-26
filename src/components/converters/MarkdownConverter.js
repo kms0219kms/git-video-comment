83881136fcdc4024d98fea06e27a8115260622bc
All PGS 모든 프로그래머들 <kms0219kms@hosue.goe.go.kr>
2020-12-26 19:53:29 +0900

    Update MarkdownConverter.js

--- src/components/converters/MarkdownConverter.js
+++ src/components/converters/MarkdownConverter.js
@@ -3,8 +3,8 @@ import Converter from '../converter/Converter'
 
 const MarkdownConverter = (props) => {
     return (
-        <Converter header="convert Youtube url to markdown">
-            <p>Hi, my name is <a href="https://sviridovserg.github.io">Sergei</a> I love delivering great product to the customer and help teams to grow! I'm using markdown files to write my blog and to put project documentation on GitLab or GitHub a lot. I find it hard to remember how to embed Youtube video to markdown files. So, I created this simple tool that converts Youtube video URI to the embeddable markup that can be simply copy-pasted to GitLab or GitHub or to my blogposts.</p>
+        <Converter header="Youtube 링크를 Markdown에 삽입하기">
+         <p>출처: <a href="https://github.com/sviridovserg/git-video-comment">sviridovserg/git-video-comment</a></p>
         </Converter>
     )
 }
